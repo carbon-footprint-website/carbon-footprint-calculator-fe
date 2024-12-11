2c0a9d665ea29b63cc92f19565253a301ef34f12
femmefatalehaein <rmfls4359@gmail.com>
2024-12-11 16:19:43 +0900

    feat : button component 제작

--- src/component/button.js
+++ src/component/button.js
@@ -2,10 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './css/button.css'
 /*...props로 추가 속성 전달 */
-const Button = ({ width = '150px', height = '50px', label, onClick, type = 'button', size = 'medium', disabled = false, ...props }) => {
-    const customStyle = { width, height };
+const Button = ({ width = '150px', height = '50px', label, onClick, type = 'button', border='1px solid white', size = 'medium', disabled = false, radius='50px',
+  bgColor='transparent', opacity='', fontSize='20px', fontWeight='400', color='white', ...props }) => {
+    const customStyle = { width, height, fontSize, fontWeight, color };
     return (
-    <button style={customStyle} {...props}
+    <button style={{... customStyle, borderRadius:radius, backgroundColor:bgColor, border:border}} {...props}
       className={`button ${size}`}
       onClick={onClick}
       type={type}
