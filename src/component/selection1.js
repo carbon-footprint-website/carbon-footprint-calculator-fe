bcbc3af518b83f1083640c7cc9bb8d33ed7c2dc7
femmefatalehaein <rmfls4359@gmail.com>
2024-12-11 16:23:31 +0900

    feat : 첫번째 선택 페이지 초기 작성

---
+++ src/component/selection1.js
@@ -0,0 +1,21 @@
+import './css/selection1.css'
+
+function Selection1 () {
+    
+    return(
+        <div className='selection-content-container'>
+            <div className='selection-inner-container'>
+                <div className='left-content-container'>
+                    어떤 교통수단을 이용해서 오셨나요?
+                </div>
+                <div className="selection-divider">
+                </div>
+                <div className='right-content-container'>
+                    right
+                </div>
+            </div>
+        </div>
+    )
+
+}
+export default Selection1;
